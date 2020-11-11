1955dea991a0830ee8e8469746a9efc9c5cae4b0
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-11-11 16:48:04 +0100

    Script para dibujar la ventana y el botón creados

---
+++ PruebaEditorPsious/Assets/EditorPsious/EditorPsious.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorPsious : EditorWindow
+{
+    [MenuItem("Window/EditorPsious")]
+    public static void ShowWindow() 
+    {
+        EditorWindow.GetWindow<EditorPsious>("EditorPsious");
+    }
+    private void OnGUI()
+    {
+        if (GUILayout.Button("CheckFiles")) 
+        {
+            
+        }
+    }
+}
