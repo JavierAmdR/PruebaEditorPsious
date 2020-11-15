bc6b680ad7399c9a1a2ef7c588b4a9bc3a355c73
JavierAmdR <javieramador_bcnvj1ma1920@cev.com>
2020-11-15 20:50:27 +0100

    Programado funcionamiento

--- Assets/EditorPsious/EditorPsious.cs
+++ Assets/EditorPsious/EditorPsious.cs
@@ -1,18 +1,45 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 public class EditorPsious : EditorWindow
 {
+    string currentLine;
+    string path;
+    string currentFolder;
+
     [MenuItem("Window/EditorPsious")]
     public static void ShowWindow() 
     {
-        EditorWindow.GetWindow<EditorPsious>("EditorPsious");
+        GetWindow<EditorPsious>("EditorPsious");
     }
     private void OnGUI()
     {
         if (GUILayout.Button("CheckFiles")) 
         {
-            
+            string filePath = "Assets/Resources/DirectoriesPsious.txt";
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++) 
+            {
+                currentLine = lines[i];
+                if (currentLine[0] != '\t') 
+                {
+                    currentFolder = lines[i] + "/";
+                    if (!Directory.Exists(path)) 
+                    {
+                        Debug.LogWarning("Error " + path + " no existe");
+                    }
+                }
+                else 
+                {
+                    path = currentFolder + lines[i].Trim() + "/";
+                    if (!Directory.Exists(path)) 
+                    {
+                        Debug.LogWarning("Error " + path + " no existe");
+                    }
+                }
+            }
         }
     }
 }
